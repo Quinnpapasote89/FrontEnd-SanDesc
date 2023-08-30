809c0bc7f9860d6053836a722e3ec24ab573986c
Quinnpapasote89 <admaor2000@gmail.com>
2023-08-30 10:12:54 -0600

    Actualizaciones de FrontEnd

--- FrontEnd-SanDesc/src/app/app-routing.module.ts
+++ FrontEnd-SanDesc/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sidebar', component:SideBarComponent},
+  {path: 'header', component:HeaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd-SanDesc/src/app/app.module.ts
+++ FrontEnd-SanDesc/src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
+import { HeaderComponent } from './header/header.component';
+import { ProductionComponent } from './production/production.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent
+    SideBarComponent,
+    HeaderComponent,
+    ProductionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
